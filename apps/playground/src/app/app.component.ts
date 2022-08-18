54e51d9d385e3b59db6c8544c8ee77631a7e12a1
jemume <jemumesan@gmail.com>
2022-08-18 11:31:44 +0200

    initial commit

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-darwin-wmf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'playground';
+}
